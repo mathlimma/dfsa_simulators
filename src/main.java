37ca1e839b17f29b8fd776efa00c4a9117834160
mathlimma <mal3@cin.ufpe.br>
2019-10-05 23:12:41 -0300

    working main file

--- src/main.java
+++ src/main.java
@@ -1,9 +1,37 @@
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Especifique quais simuladores você deseja utilizar: \n"
+				+ "1 - Lower Bound \n"
+				+ "2 -  Eom-Lee \n"
+				+ "3 - Ambos");
+		int protocolsChoosen = in.nextInt();
+		System.out.println("Especifique o número de inicial de etiquetas: ");
+		int inicialNumberTags = in.nextInt();
+		System.out.println("Especifique o número de incremento de etiquetas: ");
+		int incrementTagsBy = in.nextInt();
+		System.out.println("Especifique o número máximo de etiquetas: ");
+		int maxNumberTags = in.nextInt();
+		System.out.println("Especifique o número de repetições por quantidade de etiquetas: ");
+		int numberRepetitions = in.nextInt();
+		System.out.println("Especifique o tamanho inicial do quadro: ");
+		int inicialFrameSize = in.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
 }
+
+
+
